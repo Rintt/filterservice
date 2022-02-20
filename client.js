76008f7275b76de4419c1252a239e203959f6890
Rintt <ufukalgac@hotmail.com>
2022-02-20 22:33:05 +0100

    changed some kinks

--- client.js
+++ client.js
@@ -1,25 +1,5 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-// require('@google-cloud/trace-agent').start();
-
 const path = require('path');
 const grpc = require('grpc');
-const leftPad = require('left-pad');
 const pino = require('pino');
 
 const PROTO_PATH = path.join(__dirname, './proto/demo.proto');
@@ -36,10 +16,10 @@ const logger = pino({
   useLevelLabels: true
 });
 
+//interface for the functions in server.js
 const request = {
     filter_code: 'kitchen',
 };
-
 client.getSupportedCategories({}, (err, response) => {
   if (err) {
     logger.error(`Error in getSupportedCategories: ${err}`);
--- server.js
+++ server.js
@@ -1,54 +1,4 @@
-/*
- * Copyright 2018 Google LLC.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// if (process.env.DISABLE_PROFILER) {
-//   console.log("Profiler disabled.")
-// }
-// else {
-//   console.log("Profiler enabled.")
-//   require('@google-cloud/profiler').start({
-//     serviceContext: {
-//       service: 'filterservice',
-//       version: '1.0.0'
-//     }
-//   });
-// }
-
-
-// if (process.env.DISABLE_TRACING) {
-//   console.log("Tracing disabled.")
-// }
-// else {
-//   console.log("Tracing enabled.")
-//   require('@google-cloud/trace-agent').start();
-// }
-
-// if (process.env.DISABLE_DEBUGGER) {
-//   console.log("Debugger disabled.")
-// }
-// else {
-//   console.log("Debugger enabled.")
-//   require('@google-cloud/debug-agent').start({
-//     serviceContext: {
-//       service: 'filterservice',
-//       version: 'VERSION'
-//     }
-//   });
-// }
-
+//setups
 const path = require('path');
 const grpc = require('@grpc/grpc-js');
 const pino = require('pino');
@@ -59,7 +9,7 @@ const HEALTH_PROTO_PATH = path.join(__dirname, './proto/grpc/health/v1/health.pr
 
 const PORT = 5001;
 
-const shopProto = _loadProto(MAIN_PROTO_PATH).hipstershop;
+// const shopProto = _loadProto(MAIN_PROTO_PATH).hipstershop;
 const healthProto = _loadProto(HEALTH_PROTO_PATH).grpc.health.v1;
 
 const logger = pino({
@@ -69,9 +19,6 @@ const logger = pino({
   useLevelLabels: true
 });
 
-/**
- * Helper function that loads a protobuf file.
- */
 function _loadProto(path) {
   const packageDefinition = protoLoader.loadSync(
     path,
@@ -85,11 +32,7 @@ function _loadProto(path) {
   );
   return grpc.loadPackageDefinition(packageDefinition);
 }
-
-/**
- * Helper function that gets currency data from a stored JSON file
- * Uses public data from European Central Bank
- */
+//getproducts as an internal function to query for the available products
 function _getProducts(callback) {
   const data = require('./data/product_catalog.json');
   console.log(data.products);
@@ -97,37 +40,22 @@ function _getProducts(callback) {
     callback(data.products);
   }
 }
-
-/**
- * Lists the supported currencies
- */
-function getSupportedCategories(products, callback) {
+//get all possible categories within the products
+function getSupportedCategories(callback) {
   logger.info('Getting supported categories...');
   cat = []
-  if (products) {
-      products.forEach(ele => ele.categories.forEach(item => cat.push(item)))
-  }
-  else {
     _getProducts((data) => {
-      // callback(null, {categories: Object.keys(data.categories)});
       data.forEach(ele => ele.categories.forEach(item => cat.push(item)))
-      
     });
-  }
   if (callback) {
   callback(cat);
   }
 }
-
-/**
- * Filters the products according to the 'from' in request
- */
+//filter the products with the given filter_code inside call
 function filter(call, callback) {
   try {
     _getProducts((data) => {
       const request = call.request;
-      // Convert: from_currency --> EUR
-      // const from = request.from;
       results = []
       data.map(ele => {
         ele.categories.includes(request.filter_code) ? results.push(ele) : null
@@ -151,15 +79,13 @@ function check(call, callback) {
   callback(null, { status: 'SERVING' });
 }
 /**
- * Starts an RPC server that receives requests for the
- * CurrencyConverter service at the sample server port
+ * Starts an RPC server
  */
 function main () {
   logger.info(`Starting gRPC server on port ${PORT}...`);
   const server = new grpc.Server();
-  server.addService(shopProto.FilterService.service, {getSupportedCategories, filter});
-    // , convert});
-  server.addService(healthProto.Health.service, {check});
+  // server.addService(shopProto.FilterService.service, {getSupportedCategories, filter});
+  // server.addService(healthProto.Health.service, {check});
 
   server.bindAsync(
     `0.0.0.0:${PORT}`,
@@ -169,7 +95,7 @@ function main () {
       server.start();
     },
    );
-  // console.log('\n')
+  // possible functions to test 
   //  _getProducts(data => console.log(data))
   //  getSupportedCategories(data => console.log(data))
   //  filter({request : {filter_code: 'kitchen'}}, data => console.log(data))
